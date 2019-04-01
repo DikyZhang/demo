d9f2800b2b696d32bcb39264450b01f6abc7fcc2
diky <729768304@qq.com>
2019-04-01 15:43:55 +0800

    IDEA整合GITHUB

---
+++ src/test/java/com/example/demo/TestGit.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+/**
+ * Created by Diky on 2019/4/1.
+ */
+public class TestGit {
+    public static void main(String [] args){
+        System.out.println("你好！");
+    }
+}
