82d31b8d7ba9a468946ef0613e902171ee1730dc
diky <729768304@qq.com>
2019-04-01 15:47:18 +0800

    IDEA整合GITHUB

--- src/test/java/com/example/demo/TestGit.java
+++ src/test/java/com/example/demo/TestGit.java
@@ -5,6 +5,6 @@ package com.example.demo;
  */
 public class TestGit {
     public static void main(String [] args){
-        System.out.println("你好！");
+        System.out.println("你好111！");
     }
 }
